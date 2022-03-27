36f523347b4699361adabfbb89fb167dda62bbda
volkovVA <volkov.slava@hotmail.com>
2022-03-27 20:30:48 +0300

    feat: add maps

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,41 @@
+// Instructions to other classes how be an argument for 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(mapDivId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
